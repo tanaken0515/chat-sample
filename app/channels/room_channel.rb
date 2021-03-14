535103338955ea360bb2a155561bf0afc68cecb4
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-03-14 16:50:56 +0900

    realtime messaging

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,12 +1,20 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from 'room_channel'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    message = Message.create!(content: data['message'])
+    ActionCable.server.broadcast('room_channel', { message: render_message(message) })
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.render(partial: 'messages/message', locals: { message: message })
   end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -2,7 +2,15 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("RoomChannel", {
   connected() {
-    // Called when the subscription is ready for use on the server
+    document.
+    querySelector('input[data-behavior="room_speaker"]').
+    addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        this.speak(event.target.value);
+        event.target.value = '';
+        return event.preventDefault();
+      }
+    });
   },
 
   disconnected() {
@@ -10,10 +18,11 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    const element = document.querySelector('#messages')
+    element.insertAdjacentHTML('beforeend', data['message'])
   },
 
-  speak: function() {
-    return this.perform('speak');
+  speak: function(message) {
+    return this.perform('speak', { message: message });
   }
 });
