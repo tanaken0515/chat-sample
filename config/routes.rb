003bc98925f08c564c5736c002fb6ba91d8f41d8
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-03-14 18:49:03 +0900

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'join', to: 'users#new', as: :join
+  post 'join', to: 'users#create'
   get 'rooms/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
