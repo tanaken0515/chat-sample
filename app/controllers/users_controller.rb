5976b244cb44b6843e55dc53edbd312f25d521fb
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-03-21 16:34:06 +0900

    login just after join

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to rooms_show_path
+      login(@user)
+      redirect_to root_path
     else
       render :new
     end
