1fe7d84dca7debaa0b64356f5c60c049be2722ca
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2021-03-22 16:59:37 +0900

    apply css for message owner

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :speaker, class_name: 'User', foreign_key: 'speaker_id', optional: true
+
+  def owned_by?(user)
+    speaker.present? && speaker == user
+  end
 end
